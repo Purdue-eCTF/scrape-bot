849b4df5e869e87f0aafa01fad8430aef787c5a7
Kevin Yu <kevinyu567@gmail.com>
2024-01-23 16:00:16 -0500

    Notify in case of scoreboard changes

--- bot.ts
+++ bot.ts
@@ -1,5 +1,5 @@
 import {ActivityType, Client, EmbedBuilder} from 'discord.js';
-import {token} from './auth';
+import {notifyChannelId, token} from './auth';
 
 
 const client = new Client({
@@ -27,6 +27,7 @@ let top5: string[] = [];
 
 
 async function fetchAndUpdateScoreboard() {
+    console.log('Re-fetching eCTF scoreboard');
     const raw = await (await fetch('https://sb.ectf.mitre.org/game/summary')).text();
 
     const tables = raw.matchAll(/<tbody class='.*?' id='.*?'>([^]+?)<\/tbody>/g);
@@ -34,16 +35,32 @@ async function fetchAndUpdateScoreboard() {
 
     lastUpdated = new Date();
     top5 = [];
+
+    const totalDiffs: string[] = []
     for (const [, raw] of tables) {
         const teams = raw.matchAll(/<tr>\s*<td>(\d+)<\/td>\s*<td class='break-word'>\s*<a href="(.+?)">(\w+)<\/a>\s*<\/td>\s*<td>(\d+)<\/td>\s*<td>(\d+)<\/td>\s*<td>[^]+?<\/td>\s*<\/tr>/g);
 
         for (const [, rank, href, name, achievements, points] of teams) {
-            console.log(rank, href, name, achievements, points);
+            const absoluteHref = `https://sb.ectf.mitre.org${href}`;
+
+            // Construct diff string if fields change
+            if (scoreboard[name]) {
+                const diffs: string[] = [];
+
+                if (scoreboard[name].points !== Number(points))
+                    diffs.push(`[points: ${scoreboard[name].points} → ${Number(points)}]`)
+                if (scoreboard[name].rank !== Number(rank))
+                    diffs.push(`[rank: ${scoreboard[name].rank} → ${Number(rank)}]`)
+                if (scoreboard[name].achievements !== Number(achievements))
+                    diffs.push(`[achievements: ${scoreboard[name].achievements} → ${Number(achievements)}]`)
+
+                if (diffs.length) totalDiffs.push(`[${name}](${absoluteHref}): ${diffs.join(' ')}`)
+            }
 
             scoreboard[name] = {
                 name,
                 rank: Number(rank),
-                href: `https://sb.ectf.mitre.org${href}`,
+                href: absoluteHref,
                 achievements: Number(achievements),
                 points: Number(points)
             }
@@ -52,6 +69,20 @@ async function fetchAndUpdateScoreboard() {
 
         isTop = false;
     }
+
+    // Broadcast diffs if they exist
+    if (!totalDiffs.length) return;
+
+    const channel = client.channels.cache.get(notifyChannelId);
+    if (!channel?.isTextBased()) return;
+
+    const diffEmbed = new EmbedBuilder()
+        .setTitle('Detected eCTF Scoreboard Diffs')
+        .setDescription(totalDiffs.join('\n'))
+        .setColor('#C61130')
+        .setTimestamp();
+
+    await channel.send({embeds: [diffEmbed]});
 }
 
 client.once('ready', async () => {
