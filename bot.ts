7c4993126a0b159f66beaaa527f7fc147143fd87
Kevin Yu <kevinyu567@gmail.com>
2024-03-03 00:20:50 -0500

    Separate broadcasting and updating diffs

--- bot.ts
+++ bot.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {ActivityType, Client, EmbedBuilder} from 'discord.js';
+
+// Modules
 import {BuildStatusUpdateReq, formatCommitShort, formatPiStatus, statusToColor} from './status';
+import {fetchAndUpdateScoreboard, lastUpdated, scoreboard, top5} from './scoreboard';
+
+// Config
 import {failureChannelId, notifyChannelId, port, statusChannelId, statusMessageId, token} from './auth';
 
 
@@ -17,65 +22,27 @@ const client = new Client({
     allowedMentions: {repliedUser: false}
 });
 
-type TeamData = {
-    name: string,
-    rank: number,
-    href: string,
-    achievements: number,
-    points: number,
-}
-const scoreboard: {[name: string]: TeamData} = {};
-let lastUpdated: Date;
-let top5: string[] = [];
 
+async function broadcastDiffs() {
+    const totalDiffs: string[] = [];
 
-async function fetchAndUpdateScoreboard() {
-    console.log('Re-fetching eCTF scoreboard');
-    const raw = await (await fetch('https://sb.ectf.mitre.org/game/summary')).text();
+    for (const team of Object.values(scoreboard)) {
+        // Construct diff string if fields change
+        const diffs: string[] = [];
 
-    const tables = raw.matchAll(/<tbody class='.*?' id='.*?'>([^]+?)<\/tbody>/g);
-    let isTop = true; // TODO: hacky?
+        if (team.prevPoints !== team.points)
+            diffs.push(`[points: ${team.prevPoints} → ${team.points}]`)
+        if (team.prevAchievements !== team.achievements)
+            diffs.push(`[achievements: ${team.prevAchievements} → ${team.achievements}]`)
 
-    lastUpdated = new Date();
-    top5 = [];
+        if (diffs.length) {
+            // Push rank only if other diffs already exist so that one team jumping 15 ranks doesn't cause
+            // 14 other lines of diffs.
+            if (team.prevRank !== team.rank)
+                diffs.push(`[rank: ${team.prevRank} → ${team.rank}]`);
 
-    const totalDiffs: string[] = []
-    for (const [, raw] of tables) {
-        const teams = raw.matchAll(/<tr>\s*<td>(\d+)<\/td>\s*<td class='break-word'>\s*<a href="(.+?)">(\w+)<\/a>\s*<\/td>\s*<td>(\d+)<\/td>\s*<td>(\d+)<\/td>\s*<td>[^]+?<\/td>\s*<\/tr>/g);
-
-        for (const [, rank, href, name, achievements, points] of teams) {
-            const absoluteHref = `https://sb.ectf.mitre.org${href}`;
-
-            // Construct diff string if fields change
-            if (scoreboard[name]) {
-                const diffs: string[] = [];
-
-                if (scoreboard[name].points !== Number(points))
-                    diffs.push(`[points: ${scoreboard[name].points} → ${Number(points)}]`)
-                if (scoreboard[name].achievements !== Number(achievements))
-                    diffs.push(`[achievements: ${scoreboard[name].achievements} → ${Number(achievements)}]`)
-
-                if (diffs.length) {
-                    // Push rank only if other diffs already exist so that one team jumping 15 ranks doesn't cause
-                    // 14 other lines of diffs.
-                    if (scoreboard[name].rank !== Number(rank))
-                        diffs.push(`[rank: ${scoreboard[name].rank} → ${Number(rank)}]`);
-
-                    totalDiffs.push(`[${name}](${absoluteHref}): ${diffs.join(' ')}`);
-                }
-            }
-
-            scoreboard[name] = {
-                name,
-                rank: Number(rank),
-                href: absoluteHref,
-                achievements: Number(achievements),
-                points: Number(points)
-            }
-            if (isTop) top5.push(name);
+            totalDiffs.push(`[${team.name}](${team.href}): ${diffs.join(' ')}`);
         }
-
-        isTop = false;
     }
 
     // Broadcast diffs if they exist
@@ -193,7 +160,7 @@ client.on('interactionCreate', async (interaction) => {
     }
 });
 
-void fetchAndUpdateScoreboard();
+void fetchAndUpdateScoreboard(true);
 setInterval(fetchAndUpdateScoreboard, 1000 * 60);
 
 void client.login(token);
---
+++ scoreboard.ts
@@ -0,0 +1,49 @@
+type TeamData = {
+    name: string,
+    rank: number,
+    href: string,
+    achievements: number,
+    points: number,
+
+    prevRank: number,
+    prevAchievements: number,
+    prevPoints: number,
+}
+export const scoreboard: {[name: string]: TeamData} = {};
+export let lastUpdated: Date;
+export let top5: string[] = [];
+
+
+export async function fetchAndUpdateScoreboard(resetDiffs: boolean = false) {
+    console.log('Re-fetching eCTF scoreboard');
+    const raw = await (await fetch('https://sb.ectf.mitre.org/game/summary')).text();
+
+    const tables = raw.matchAll(/<tbody class='.*?' id='.*?'>([^]+?)<\/tbody>/g);
+    let isTop = true; // TODO: hacky?
+
+    lastUpdated = new Date();
+    top5 = [];
+
+    for (const [, raw] of tables) {
+        const teams = raw.matchAll(/<tr>\s*<td>(\d+)<\/td>\s*<td class='break-word'>\s*<a href="(.+?)">(\w+)<\/a>\s*<\/td>\s*<td>(\d+)<\/td>\s*<td>(\d+)<\/td>\s*<td>[^]+?<\/td>\s*<\/tr>/g);
+
+        for (const [, rank, href, name, achievements, points] of teams) {
+            const absoluteHref = `https://sb.ectf.mitre.org${href}`;
+
+            scoreboard[name] = {
+                name,
+                rank: Number(rank),
+                href: absoluteHref,
+                achievements: Number(achievements),
+                points: Number(points),
+
+                prevRank: resetDiffs ? Number(rank) : scoreboard[name].prevRank,
+                prevAchievements: resetDiffs ? Number(achievements) : scoreboard[name].prevAchievements,
+                prevPoints: resetDiffs ? Number(points) : scoreboard[name].prevPoints
+            }
+            if (isTop) top5.push(name);
+        }
+
+        isTop = false;
+    }
+}
