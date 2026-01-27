1f617591d0fb276db9595f70ccfcada115403ba2
Kevin Yu <kevinyu567@gmail.com>
2026-01-27 01:00:26 -0500

    Redo build status with better typing, etc.

--- modules/status.ts
+++ modules/status.ts
@@ -1,25 +1,9 @@
-import express from 'express';
-import bodyParser from 'body-parser';
 import { EmbedBuilder } from 'discord.js';
 import { client } from '../bot';
 
 // Config
 import { DESIGN_REPO_URL, FAILURE_CHANNEL_ID, STATUS_CHANNEL_ID, STATUS_MESSAGE_ID } from '../config';
 
-
-export const server = express();
-
-server.use(bodyParser.json());
-server.post('/', async (req, res) => {
-    console.log(`[BUILD] Received webhook push:\n${JSON.stringify(req.body)}`);
-    try {
-        await updateBuildStatus(req.body);
-        res.status(200).json({ ok: true });
-    } catch (e) {
-        console.error(e);
-        res.status(400).json({ ok: false });
-    }
-});
 
 async function updateBuildStatus(req: BuildStatusUpdateReq) {
     const channel = client.channels.cache.get(STATUS_CHANNEL_ID);
@@ -30,40 +14,39 @@ async function updateBuildStatus(req: BuildStatusUpdateReq) {
         ?? await channel.messages.fetch(STATUS_MESSAGE_ID)
         ?? channel.lastMessage;
 
-    const queueStatus = req.build.queue.map((d, i) => `${i + 1}. ${formatCommitShort(d)}`).join('\n')
+    const queueStatus = req.queue.map((d, i) => `${i + 1}. ${formatActionShort(d)}`).join('\n')
         || '*No commits queued.*'
-    const piStatus = req.test.activeTests.map((s, i) => `${i + 1}. ${formatPiStatus(s)}`).join('\n');
-    const buildStatus = req.build.active
-        ? formatCommitShort(req.build.active)
-        : '*No commits loaded.*'
+    const activeStatus = req.active.map((d, i) => `${i + 1}. ${formatActionShort(d)}`).join('\n')
+        || '*No commits loaded.*'
 
-    const color = req.status
-        ? statusToColor(req.status)
-        : '#27272a'
+    // Take the highest severity status among the active jobs for the embed color
+    const status = req.active.reduce<ActionStatus | null>((s, c) => {
+        if (!s) return c.status;
+        if (s === 'SUCCESS' || c.status === 'BUILD_FAILED' || c.status === 'TEST_FAILED')
+            return c.status;
+        return s;
+    }, null);
 
     const statusEmbed = new EmbedBuilder()
         .setTitle('Secure design build status')
-        .setDescription(`**Status:** ${req.status || 'N/A'}`)
         .addFields(
-            { name: 'Pis', value: piStatus },
-            { name: 'Building:', value: buildStatus },
+            { name: 'Active:', value: activeStatus },
             { name: 'Queued:', value: queueStatus }
         )
-        .setColor(color)
+        .setColor(status ? statusToColor(status) : '#27272a')
         .setTimestamp()
 
-    // Report build failures to the appropriate channel
-    if (
-        (req.update.type === 'BUILD' || req.update.type === 'TEST')
-        && req.update.state.result === 'FAILED'
-    ) {
-        const runHref = `${DESIGN_REPO_URL}/actions/runs/${req.update.state.commit.runId}`;
+    // Report all build / test failures to the appropriate channel;  this assumes that each
+    // build / test failure is sent only once to Tom.
+    const failures = req.active.filter((r) => r.status === 'BUILD_FAILED' || r.status === 'TEST_FAILED');
+    for (const failed of failures) {
+        const runHref = `${DESIGN_REPO_URL}/actions/runs/${failed.commit.runId}`;
 
         const failureChannel = client.channels.cache.get(FAILURE_CHANNEL_ID);
         const failureEmbed = new EmbedBuilder()
-            .setTitle(`${req.update.type === 'BUILD' ? 'Build' : 'Tests'} failed for commit`)
+            .setTitle(`${failed.status === 'BUILD_FAILED' ? 'Build' : 'Tests'} failed for commit`)
             .setColor(0xb50300)
-            .setDescription(`[\`${req.update.state.commit.hash.slice(0, 7)}\`]: ${req.update.state.commit.name} (@${req.update.state.commit.author})\n[[Jump to failed workflow]](${runHref})`)
+            .setDescription(`[\`${failed.commit.hash.slice(0, 7)}\`]: ${failed.commit.name} (@${failed.commit.author})\n[[Jump to failed workflow]](${runHref})`)
             .setTimestamp()
 
         if (failureChannel?.isSendable())
@@ -74,78 +57,47 @@ async function updateBuildStatus(req: BuildStatusUpdateReq) {
     return message.edit({ embeds: [statusEmbed] });
 }
 
-type ActionStatus = 'SUCCESS' | 'TESTING' | 'BUILDING' | 'PENDING' | 'FAILED';
+type ActionStatus = 'SUCCESS' | 'TESTING' | 'BUILDING' | 'BUILD_PENDING' | 'TEST_PENDING' | 'BUILD_FAILED' | 'TEST_FAILED';
 
 type ActionResult = {
-    result: ActionStatus,
+    status: ActionStatus,
     commit: {
         hash: string,
         name: string,
         author: string,
         runId: string,
     },
-    actionStart: number // epoch s
-}
-type PiStatus = {
-    ip: string,
-    locked: boolean,
-    active?: ActionResult
-}
-type BuildStatusUpdateReq = {
-    status?: ActionStatus,
-    update: QueueUpdate | BuildUpdate | TestUpdate
-    build: {
-        active?: ActionResult
-        queue: ActionResult[]
-    }
-    test: {
-        activeTests: PiStatus[]
-        queue: ActionResult[]
-    }
+    start: number // epoch s
 }
 
-type QueueUpdate = {
-    type: 'QUEUE'
-}
-type BuildUpdate = {
-    type: 'BUILD',
-    state: ActionResult
-}
-type TestUpdate = {
-    type: 'TEST',
-    state: ActionResult
+type BuildStatusUpdateReq = { // <- This is the type of the object published by Build Server whenever anything changes
+    active: ActionResult[],
+    queue: ActionResult[]
 }
 
-function formatPiStatus(s: PiStatus) {
-    const status = s.locked ? (
-        '*Locked by user.*'
-    ) : !s.active ? (
-        '*No images loaded.*'
-    ) : (
-        `*Status ${s.active.result} for commit*\n${formatCommitShort(s.active)}\n`
-    );
-    return `\`${s.ip}\`: ${status}`;
-}
-
-function formatCommitShort(c: ActionResult) {
+function formatActionShort(c: ActionResult) {
     const runHref = `${DESIGN_REPO_URL}/actions/runs/${c.commit.runId}`;
-    const ts = Math.floor(c.actionStart);
+    const ts = Math.floor(c.start);
 
-    return `\\${statusToCircle(c.result)} [[\`${c.commit.hash.slice(0, 7)}\`]](${runHref}): ${c.commit.name} (@${c.commit.author}) updated <t:${ts}:R>`;
+    return `\\${statusToCircle(c.status)} [[\`${c.commit.hash.slice(0, 7)}\`]](${runHref}): ${c.commit.name} (@${c.commit.author}) updated <t:${ts}:R>`;
 }
 
-function statusToColor(status: ActionResult['result']) {
+function statusToColor(status: ActionStatus) {
     switch (status) {
         case 'SUCCESS': return 0x79ff3b;
-        case 'FAILED': return 0xb50300;
+        case 'BUILD_FAILED':
+        case 'TEST_FAILED':
+            return 0xb50300;
         default: return 0xf6b40c;
     }
 }
 
-function statusToCircle(status: ActionResult['result']) {
+function statusToCircle(status: ActionStatus) {
     switch (status) {
         case 'SUCCESS': return '🟢';
-        case 'FAILED': return '🔴';
+        case 'BUILD_FAILED':
+        case 'TEST_FAILED':
+            return '🔴';
         default: return '🟡';
     }
 }
