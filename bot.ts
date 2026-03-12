26046cffbf9657ac0d228e651bc2dc92073c3f41
Kevin Yu <kevinyu567@gmail.com>
2026-03-12 17:37:40 -0400

    Fix import issue with `bot.ts`

--- bot.ts
+++ bot.ts
@@ -29,16 +29,17 @@ export const client = new Client({
     allowedMentions: { repliedUser: false }
 });
 
-// Load commands
 client.commands = new Collection();
-for (const command of commands) {
-    client.commands.set(command.data.name, command);
-}
 
 let broadcastDiffsJob: CronJob;
 
 client.once('clientReady', async () => {
     console.log(`[DISC] Logged in as ${client.user?.tag}!`);
+
+    // Load commands
+    for (const command of commands) {
+        client.commands.set(command.data.name, command);
+    }
 
     // Broadcast diffs daily
     broadcastDiffsJob = CronJob.from({
